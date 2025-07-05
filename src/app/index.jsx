c1ffc4c1f221a865481c079d0b594ab335626bd5
Álvaro José <josealvaro3421@gmail.com>
2025-07-04 22:30:03 -0300

    Added: first high fidelity model for search bar

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,12 +1,58 @@
-import { TextInput, View } from "react-native"
+import { Image, StyleSheet, TextInput, View } from "react-native"
+import searchIcon from "../../assets/images/icons/SearchIcon.png"
 
 
 export default function Index(){
     return(
-        <View>
-            <View>
-                <TextInput>Buscar Locais</TextInput>
+        <View style={style.container}>
+            <View style={style.searchContainer}>
+                <TextInput style={style.label}>Buscar local...</TextInput>
+
+                <Image source={searchIcon} style={style.serchIcon}/>
             </View>
         </View>
     )
 }
+
+const style = StyleSheet.create(
+    {
+        container: {
+            flex: 1,
+            justifyContent: 'top',
+            alignItems: 'center',
+
+            padding: 30,
+            gap: 20,
+
+            backgroundColor: '#171717'
+        },
+
+        searchContainer: {
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            flexDirection: 'row',
+
+            padding: 10, 
+            marginTop: 10,
+
+            borderRadius: 15,
+
+            width: '100%',
+            backgroundColor: '#212121'
+        },
+
+        label: {
+            fontSize: 16,
+            fontWeight: 400,
+
+            color: '#989898'
+        },
+        
+        serchIcon: {
+            width: 25,
+            height: 25,
+            
+            color: '#989898'
+        }
+    }
+)
